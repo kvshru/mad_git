e2f240a918900bbd703f23220eb7c6599f228ded
kvsh <kvsh443@gmail.com>
2019-07-18 12:25:55 +0530

    First Commit

---
+++ app/src/main/java/com/kvsh/sliit/mygitapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.kvsh.sliit.mygitapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView1);
+        textView.setText("OwO");
+    }
+}
